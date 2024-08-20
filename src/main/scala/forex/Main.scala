f51ceba79a3b9e0c024fe74f4deca37a90a8d4ce
zakariaandy <zakariaandy.id@gmail.com>
2024-08-20 23:42:58 +0700

    Initial commit - Initialize scaffold application - Added .gitignore file

---
+++ src/main/scala/forex/Main.scala
@@ -0,0 +1,28 @@
+package forex
+
+import scala.concurrent.ExecutionContext
+import cats.effect._
+import forex.config._
+import fs2.Stream
+import org.http4s.blaze.server.BlazeServerBuilder
+
+object Main extends IOApp {
+
+  override def run(args: List[String]): IO[ExitCode] =
+    new Application[IO].stream(executionContext).compile.drain.as(ExitCode.Success)
+
+}
+
+class Application[F[_]: ConcurrentEffect: Timer] {
+
+  def stream(ec: ExecutionContext): Stream[F, Unit] =
+    for {
+      config <- Config.stream("app")
+      module = new Module[F](config)
+      _ <- BlazeServerBuilder[F](ec)
+            .bindHttp(config.http.port, config.http.host)
+            .withHttpApp(module.httpApp)
+            .serve
+    } yield ()
+
+}
