bf7d3601ab5ed9c03101ce46738fc8712319c66a
zakariaandy <zakariaandy.id@gmail.com>
2024-08-25 11:20:10 +0700

    Add mockito dependency for test

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,4 +1,4 @@
-import sbt._
+import sbt.*
 
 object Dependencies {
 
@@ -14,20 +14,21 @@ object Dependencies {
     val logback        = "1.2.3"
     val scalaCheck     = "1.15.3"
     val scalaTest      = "3.2.7"
+    val mockito        = "1.16.46"
     val catsScalaCheck = "0.3.2"
   }
 
   object Libraries {
-    def circe(artifact: String): ModuleID  = "io.circe"   %% artifact % Versions.circe
-    def http4s(artifact: String): ModuleID = "org.http4s" %% artifact % Versions.http4s
+    def circe(artifact: String): ModuleID      = "io.circe"       %% artifact % Versions.circe
+    def http4s(artifact: String): ModuleID     = "org.http4s"     %% artifact % Versions.http4s
 
     lazy val cats       = "org.typelevel" %% "cats-core"   % Versions.cats
     lazy val catsEffect = "org.typelevel" %% "cats-effect" % Versions.catsEffect
     lazy val fs2        = "co.fs2"        %% "fs2-core"    % Versions.fs2
 
     lazy val http4sDsl       = http4s("http4s-dsl")
-    lazy val http4sClient    = http4s("http4s-blaze-client")
     lazy val http4sServer    = http4s("http4s-blaze-server")
+    lazy val http4sClient    = http4s("http4s-blaze-client")
     lazy val http4sCirce     = http4s("http4s-circe")
     lazy val circeCore       = circe("circe-core")
     lazy val circeGeneric    = circe("circe-generic")
@@ -42,9 +43,10 @@ object Dependencies {
     lazy val logback = "ch.qos.logback" % "logback-classic" % Versions.logback
 
     // Test
-    lazy val scalaTest      = "org.scalatest"     %% "scalatest"       % Versions.scalaTest
-    lazy val scalaCheck     = "org.scalacheck"    %% "scalacheck"      % Versions.scalaCheck
-    lazy val catsScalaCheck = "io.chrisdavenport" %% "cats-scalacheck" % Versions.catsScalaCheck
+    lazy val scalaTest      = "org.scalatest"     %% "scalatest"               % Versions.scalaTest
+    lazy val mockito        = "org.mockito"       %% "mockito-scala-scalatest" % Versions.mockito
+    lazy val scalaCheck     = "org.scalacheck"    %% "scalacheck"              % Versions.scalaCheck
+    lazy val catsScalaCheck = "io.chrisdavenport" %% "cats-scalacheck"         % Versions.catsScalaCheck
   }
 
 }
