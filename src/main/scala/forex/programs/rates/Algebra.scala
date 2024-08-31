a5f6cb13e68676076dde87f2a1b2d30fd6a41a03
zakariaandy <zakariaandy.id@gmail.com>
2024-08-31 14:34:49 +0700

    Change Error to ProgramError

--- src/main/scala/forex/programs/rates/Algebra.scala
+++ src/main/scala/forex/programs/rates/Algebra.scala
@@ -1,8 +1,8 @@
 package forex.programs.rates
 
 import forex.domain.Rate
-import errors._
+import forex.programs.rates.errors.ProgramError
 
 trait Algebra[F[_]] {
-  def get(request: Protocol.GetRatesRequest): F[Error Either Rate]
+  def get(request: Protocol.GetRatesRequest): F[ProgramError Either Rate]
 }
