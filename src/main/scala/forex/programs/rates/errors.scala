56bdb8c6d376453f451c7f17a303b64c9ff6afb8
zakariaandy <zakariaandy.id@gmail.com>
2024-09-02 23:21:25 +0700

    Error mapping

--- src/main/scala/forex/programs/rates/errors.scala
+++ src/main/scala/forex/programs/rates/errors.scala
@@ -7,12 +7,13 @@ object errors {
   sealed trait ProgramError extends Exception
   object ProgramError {
     final case class RateLookupFailed(msg: String) extends ProgramError
+    final case class RateOutdatedError(msg: String) extends ProgramError
+    final case class RateParseError(msg: String) extends ProgramError
   }
 
   def toProgramError(error: ServiceError): ProgramError = error match {
-    case ServiceError.OneFrameLookupFailed(msg) => ProgramError.RateLookupFailed(msg)
-    case ServiceError.RateNotFoundError(msg) => ProgramError.RateLookupFailed(msg) //todo: fix this dummy parses
-    case ServiceError.RateOutdatedError(msg) => ProgramError.RateLookupFailed(msg) //todo: fix this dummy parses
-    case ServiceError.RateParseError(msg) => ProgramError.RateLookupFailed(msg) //todo: fix this dummy parses
+    case ServiceError.RateNotFoundError(msg) => ProgramError.RateLookupFailed(msg)
+    case ServiceError.RateOutdatedError(msg) => ProgramError.RateOutdatedError(msg)
+    case ServiceError.RateParseError(msg)    => ProgramError.RateParseError(msg)
   }
 }
