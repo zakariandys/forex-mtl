05f40fe506da235ff4d18a68d96ba0a7b275cc07
zakariaandy <zakariaandy.id@gmail.com>
2024-09-02 23:21:44 +0700

    Remove unused error code

--- src/main/scala/forex/services/rates/errors.scala
+++ src/main/scala/forex/services/rates/errors.scala
@@ -4,7 +4,6 @@ object errors {
 
   sealed trait ServiceError
   object ServiceError {
-    final case class OneFrameLookupFailed(msg: String) extends ServiceError
     final case class RateOutdatedError(msg: String) extends ServiceError
     final case class RateParseError(msg: String) extends ServiceError
     final case class RateNotFoundError(msg: String) extends ServiceError
